1bab5b693d85db3835d5d07e22969ce63b0394c8
Sai Akhil <matsy@users.noreply.github.com>
2022-04-23 12:29:47 -0600

    merged files for feature 1 end to end

--- rest/rest-server.py
+++ rest/rest-server.py
@@ -18,6 +18,7 @@ from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from functools import reduce
 from datetime import datetime
 import time
 import pika
@@ -37,8 +38,58 @@ jwt = JWTManager(app)
 rabbitMQHost = os.getenv("RABBITMQ_HOST") or "localhost"
 print("Connecting to rabbitmq({})".format(rabbitMQHost))
 
+###############################################################
+#               Neerab's Redis code start                     #
+###############################################################
+from collections import defaultdict
+redisHost = os.getenv("REDIS_HOST") or "localhost"
+db = redis.Redis(host=redisHost, db=1)  
+print("Connecting to redis({})".format(redisHost))
+json_file_names = os.listdir('metadata')
 
+# Remove the 5 char .json file ending to isolate hashtag name
+hashtags = [hashtag[:-5] for hashtag in json_file_names]
 
+# remove '.DS_', '.ipynb_checkp'
+non_hashtags = ['.DS_', '.ipynb_checkp']
+for non_hashtag in non_hashtags:
+    try:
+        hashtags.remove(non_hashtag)
+    except:
+        pass # If we can't remove it, it's already gone
+
+# convert hashtag metadata into dataframe
+hashtag_metadata = []
+for hashtag in hashtags: 
+    hashtag_metadata.append(pd.read_json(f'metadata/{hashtag}.json'))
+hashtag_metadata = reduce(lambda x, y: pd.concat([x, y]), hashtag_metadata)
+pd.DataFrame.reset_index(hashtag_metadata, drop=True, inplace=True)
+hashtag_metadata.tail()
+
+#### store the tag data in redis, so that we can use it as a cache.
+def store_tag_data(hashtags):
+    tag_recommender = {}
+    #print(hashtag_metadata)
+    for hashtag in hashtags:
+        df = hashtag_metadata.loc[hashtag_metadata['search_hashtag'] == hashtag]
+        hash_tags = defaultdict(int)
+        for tags in df['hashtags']:
+            for tag in tags:
+                hash_tags[tag] += 1
+        tag_recommender[hashtag] = sorted(hash_tags.items(), key=lambda item: item[1], reverse=True)[:15]
+    #print(tag_recommender)
+    return tag_recommender
+
+def store_tag_redis(db):
+    tag_recommender = store_tag_data(hashtags)
+    #print(tag_recommender)
+    ### Push the data into redis so, that we can take hashtags from cache ####
+    #### push the username and password and activites into redis cache
+    for hashtag in hashtags:
+        db.set(hashtag,pickle.dumps(tag_recommender[hashtag]))
+###############################################################
+#               Neerab's Redis code end                       #
+###############################################################
 
 
 def enqueueDataToLogsExchange(message,messageType):
@@ -63,8 +114,6 @@ def enqueueDataToLogsExchange(message,messageType):
 
     rabbitMQChannel.close()
     rabbitMQ.close()
-
-
 
 class enqueueWorker(object):
     def __init__(self):
@@ -102,6 +151,23 @@ class enqueueWorker(object):
 
 @app.route('/api/upload/captionimage', methods=['POST'])
 def handle_captionform():
+    # Adding code for rest handling - Trail
+    print("###############################################################")
+    print("#               Redis Code called                             #")
+    print("###############################################################")
+
+    store_tag_redis(db)
+    hashtag = "travel"
+    details = pickle.loads(db.get(hashtag))
+
+    import re
+    recommendation = []
+    print(details)
+    for tag in details:
+        recommendation.append(tag[0])
+    print(json.dumps(recommendation))
+    return json.dumps(recommendation)
+
     try:
         print(" Inside Caption API Upload ")
         enqueueDataToLogsExchange('Call to api /api/upload/captionimage','info')
